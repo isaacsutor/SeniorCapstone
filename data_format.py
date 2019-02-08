23bbb302231f43802db80e769d2abdd2b085cf32
isaacsutor <isaac.sutor@gmail.com>
2019-02-08 00:06:17 -0500

    maybe runs ok

--- data_format.py
+++ data_format.py
@@ -44,6 +44,29 @@ class DataSet(object):
             ]
 
         # split into groups of num_steps
+        X = np.array([seq[i: i + self.num_steps] for i in range(len(seq) - self.num_steps)])
+        y = np.array([seq[i + self.num_steps] for i in range(len(seq) - self.num_steps)])
+
+        train_size = int(len(X) * (1.0 - self.test_ratio))
+        train_X, test_X = X[:train_size], X[train_size:]
+        train_y, test_y = y[:train_size], y[train_size:]
+        return train_X, train_y, test_X, test_y
+
+    def generate_one_epoch(self, batch_size):
+        num_batches = int(len(self.train_X)) // batch_size
+        if batch_size * num_batches < len(self.train_X):
+            num_batches += 1
+
+        batch_indices = range(num_batches)
+        random.shuffle(batch_indices)
+        for j in batch_indices:
+            batch_X = self.train_X[j * batch_size: (j + 1) * batch_size]
+            batch_y = self.train_y[j * batch_size: (j + 1) * batch_size]
+            assert set(map(len, batch_X)) == {self.num_steps}
+            yield batch_X, batch_y
+
+
+
 
 
 
