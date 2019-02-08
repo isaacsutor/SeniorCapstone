011c60bef3bc609453085bc66eda743daf12a802
isaacsutor <isaac.sutor@gmail.com>
2019-02-07 23:44:18 -0500

    setting up data config

---
+++ NeuralNetworkTryOne.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+import pandas as pd
+import random
+import time
+
+random.seed(time.time())
+
+class DataSet(object):
+    def __init__(self, stock_ID, input_size=1, num_steps=30,
+                 test_ratio=0.1, normalized=True, close_price_only=True):
+        self.stock_ID = stock_ID
+        self.input_size = input_size
+        self.num_steps = num_steps
+        self.test_ratio = test_ratio
+        self.close_price_only = close_price_only
+        self.normalized = normalized
+
+        # read csv file
+        raw_df = pd.read_csv(os.path.join("data", "%s.csv" % stock_ID))
+
+        # merge into one sequence
