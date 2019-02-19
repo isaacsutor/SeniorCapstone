5cf91694dfe112112a0792778167f8b40ed84167
isaac.sutor.15 <isaac.sutor.15@cnu.edu>
2019-02-18 23:01:27 -0500

    added layers with weights and biases. 4 layers and 1 out

--- NeuralNetWithDat.py
+++ NeuralNetWithDat.py
@@ -35,3 +35,42 @@ X_train = data_train[:, 1:]
 y_train = data_train[:, 0]
 X_test = data_test[:, 1:]
 y_test = data_test[:, 0]
+
+# n_stocks = X_train.shape[1]
+
+
+# x contains the network inputs
+# y contains the outputs
+X = tf.placeholder(dtype=tf.float32, shape=[None, n_stocks])
+Y = tf.placeholder(dtype=tf.float32, shape=[None])
+
+
+# Parameters
+n_stocks = 500
+n_neurons_1 = 1024
+n_neurons_2 = 512
+n_neurons_3 = 256
+n_neurons_4 = 128
+n_target = 1
+
+
+# Layer 1
+W_hidden_1 = tf.Variable(weight_initializer([n_stocks, n_neurons_1]))
+bias_hidden_1 = tf.Variable(bias__initializer([n_neurons_1]))
+
+# Layer 2
+W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
+bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]))
+
+# Layer 3
+W_hidden_3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
+bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]))
+
+# Layer 4
+W_hidden_4 = tf.Variable(weight_initializer([n_neurons_3, n_neurons_4]))
+bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
+
+# Output Layer
+W_out = tf.Variable(weight_initializer([n_neurons_4, n_target]))
+bias_out = tf.Variable(bias_initializer([n_target]))
+
