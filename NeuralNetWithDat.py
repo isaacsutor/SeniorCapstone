6f44964f871140a1c74455f23887b5392e1e8a00
isaac.sutor.15 <isaac.sutor.15@cnu.edu>
2019-02-19 00:55:10 -0500

    works maybe. about to train

--- NeuralNetWithDat.py
+++ NeuralNetWithDat.py
@@ -20,13 +20,13 @@ data = data.values
 # Separate Training and Test Data
 train_start = 0
 train_end = int(np.floor(0.8*n))
-test_start = train_end
+test_start = train_end + 1
 test_end = n
 data_train = data[np.arange(train_start, train_end), :]
 data_test = data[np.arange(test_start, test_end), :]
 
 # Scale data
-scaler = MinMaxScaler()
+scaler = MinMaxScaler(feature_range=(-1, 1))
 data_train = scaler.fit_transform(data_train)
 data_test = scaler.transform(data_test)
 
@@ -36,8 +36,6 @@ y_train = data_train[:, 0]
 X_test = data_test[:, 1:]
 y_test = data_test[:, 0]
 
-# n_stocks = X_train.shape[1]
-
 
 # x contains the network inputs
 # y contains the outputs
@@ -46,6 +44,7 @@ Y = tf.placeholder(dtype=tf.float32, shape=[None])
 
 
 # Parameters
+# n_stocks = X_train.shape[1]
 n_stocks = 500
 n_neurons_1 = 1024
 n_neurons_2 = 512
@@ -53,10 +52,15 @@ n_neurons_3 = 256
 n_neurons_4 = 128
 n_target = 1
 
+# Initializer
+sigma = 1
+weight_initializer = tf.variance_scaling_initializer(mode="fan_avg",
+                                                     distribution="uniform", scale=sigma)
+bias_initializer = tf.zeros_initializer()
 
 # Layer 1
 W_hidden_1 = tf.Variable(weight_initializer([n_stocks, n_neurons_1]))
-bias_hidden_1 = tf.Variable(bias__initializer([n_neurons_1]))
+bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]))
 
 # Layer 2
 W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
@@ -73,4 +77,73 @@ bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
 # Output Layer
 W_out = tf.Variable(weight_initializer([n_neurons_4, n_target]))
 bias_out = tf.Variable(bias_initializer([n_target]))
+
+
+
+# Hidden Layer
+hidden_1 = tf.nn.relu(tf.add(tf.matmul(X, W_hidden_1), bias_hidden_1))
+hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
+hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
+hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
+
+# Output Layer (to be transposed)
+out = tf.transpose(tf.add(tf.matmul(hidden_4, W_out), bias_out))
+
+# Cost Function (Mean Squared Error)
+mse = tf.reduce_mean(tf.squared_difference(out, Y))
+
+# Optimizer (adapts network weight and bias variable)
+opt = tf.train.AdamOptimizer().minimize(mse)
+
+# Make Session
+net = tf.Session()
+
+# Run Initializer
+net.run(tf.global_variables_initializer())
+
+# Setup Interactive Plot
+plt.ion()
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(y_test)
+line2, = ax1.plot(y_test*0.5)
+plt.show()
+
+# Number of epochs and batch size
+epochs = 10
+batch_size = 256
+mse_train = []
+mse_test = []
+
+for e in range(epochs):
+    # Shuffle training data
+    shuffle_indices = np.random.permutation(np.arrange(len(y_train)))
+    X_train = X_train[shuffle_indices]
+    y_train = y_train[shuffle_indices]
+
+    # Minibatch training
+    for i in range(0, len(y_train) // batch_size): # batch_size
+        start = i * batch_size
+        batch_x = X_train[start:start + batch_size]
+        batch_y = y_train[start:start + batch_size]
+        # Run optimizer with batch
+        net.run(opt, feed_dict={X: batch_x, Y: batch_y})
+
+        # Show progress
+        if np.mod(i, 50) == 0:
+            mse_train.append(net.run(mse, feed_dict={X: X_train, Y: y_train}))
+            mse_test.append(net.run(mse, feed_dict={X: X_test, Y: y_test}))
+            print('MSE Train: ', mse_train[-1])
+            print('MSE Test: ', mse_test[-1])
+            # Prediction
+            pred = net.run(out, feed_dict={X: X_test})
+            line2.set_ydata(pred)
+            plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
+            file_name = 'img/epoch_' + str(e) + '_batch_' + str(i) + '.jpg'
+            plt.savefig(file_name)
+            plt.pause(0.01)
+
+# Print final MSE after Training
+# mse_final = net.run(mse, feed_dict={X: X_test, Y: y_test})
+# print(mse_final)
 
