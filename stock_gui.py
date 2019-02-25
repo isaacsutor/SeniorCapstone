e9a6ab87d208e0141cb6c0c8b1be23e7750578bb
isaac.sutor.15 <isaac.sutor.15@cnu.edu>
2019-02-24 22:21:57 -0500

    added file for suggested basic PyQt folder structure

--- stock_gui.py
+++ stock_gui.py
@@ -1,8 +1,78 @@
 import sys
 import PyQt5
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp, QMenu
+from PyQt5.QtWidgets import QMainWindow, QApplication, \
+    QAction, qApp, QMenu, QLabel, QPushButton, QHBoxLayout, \
+    QVBoxLayout, QWidget
 from PyQt5.QtGui import QIcon
 
 
 # DO the things
+class Example(QWidget): #QMainWindow
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        # exitAct = QAction(QIcon('exitpic.png'), ' &Exit', self)
+        # exitAct.setShortcut('Ctrl+Q')
+        # exitAct.setStatusTip('Exit application')
+        # exitAct.triggered.connect(qApp.quit)
+
+        # self.statusbar = self.statusBar()
+        # self.statusbar.showMessage('Ready')
+
+        # menubar = self.menuBar()
+
+        # fileMenu = menubar.addMenu(' &File')
+        # fileMenu.addAction(exitAct)
+
+        # impMenu = QMenu('Import', self)
+        # impAct = QAction('Import mail', self)
+        # impMenu.addAction(impAct)
+
+        # newAct = QAction('New', self)
+        # fileMenu.addAction(newAct)
+        # fileMenu.addMenu(impMenu)
+
+        # viewMenu = menubar.addMenu('View')
+        # viewStatAct = QAction('View statusbar', self, checkable=True)
+        # viewStatAct.setStatusTip('View statusbar')
+        # viewStatAct.setChecked(True)
+        # viewStatAct.triggered.connect(self.toggleMenu)
+
+        # viewMenu.addAction(viewStatAct)
+
+        # self.setGeometry(300, 300, 250, 150)
+        # self.setWindowTitle('Statusbar')
+        # self.show()
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Buttons')
+        self.show()
+
+    def toggleMenu(self, state):
+        if state:
+            self.statusbar.show()
+        else:
+            self.statusbar.hide()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
 
